b8b454312ab981832aec76965023b7b3fcaa8307
tthdvd88 <tthdvd88@gmail.com>
2024-11-19 10:33:27 +0100

    Fragments for ui

---
+++ app/src/main/java/com/example/itinventoryappat/ui/SettingsFragment.java
@@ -0,0 +1,26 @@
+package com.example.itinventoryappat.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.itinventoryappat.R;
+
+
+public class SettingsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        return view;
+    }
+}
